3019e84c1351e7a0715561e63ea35a25d185eec6
Yael Charles <yaelcharles417@gmail.com>
2025-10-12 21:30:56 -0600

    [Feature] lista de pokemon con 5 elementos mock para probar contenido

--- app/src/main/java/com/app/pokedexapp/presentation/screens/home/components/PokemonListTab.kt
+++ app/src/main/java/com/app/pokedexapp/presentation/screens/home/components/PokemonListTab.kt
@@ -1,7 +1,27 @@
 package com.app.pokedexapp.presentation.screens.home.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun PokemonListTab(onPokemonClick: (String) -> Unit) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        // Mock data para el lab3
+        items(5) { index ->
+            PokemonCard(
+                name = "Pokemon ${index + 1}",
+                imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png",
+                onClick = { onPokemonClick(index.toString()) },
+            )
+        }
+    }
 }
